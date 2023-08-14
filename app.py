accee54e8eb62b433b1aa437980de4584ab4bd34
qxnam <quachnam311@gmail.com>
2023-08-14 16:08:10 +0700

    update streamlit

--- app.py
+++ app.py
@@ -3,35 +3,55 @@ import pickle
 import streamlit as st
 from classifier import Classifier
 
-loaded_model = None
+st.set_page_config(
+    page_title="Flower Classifier App",
+    page_icon="🌻",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': '''Quách Xuân Nam - 20020541 - IUH\n
+        https://www.facebook.com/20020541.nam'''
+    }
+)
+model = None
 with open('best_model.pkl', 'rb') as f:
-    loaded_model = pickle.load(f)
+    model = pickle.load(f)
 
-def prediction(model, sepal_length,sepal_width,petal_length,petal_width):
-    X_new = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
-    X_new = X_new.reshape(1,-1)
+def predict(features):
+    decode = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+    X_new = np.array([features]).reshape(1,-1)
     prediction = model.predict(X_new)
-    if prediction == 0:
-        return 'This is Iris-setosa','https://upload.wikimedia.org/wikipedia/commons/5/56/Kosaciec_szczecinkowaty_Iris_setosa.jpg'
-    elif prediction == 1:
-        return 'This is Iris-versicolor','https://upload.wikimedia.org/wikipedia/commons/4/41/Iris_versicolor_3.jpg'
-    else:
-        return 'This is Iris-virginica' , 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Iris_virginica.jpg'
+    print(prediction)
+    return decode[prediction[0]]
+    
+def input_features():
+    sepal_length = st.sidebar.number_input('Sepal length', min_value=4.3, max_value=7.9, value=5.4, step=0.1)
+    # sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    
+    sepal_width = st.sidebar.number_input('Sepal width', min_value=2.0, max_value=4.4, value=3.4, step=0.1)
+    # sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    
+    petal_length = st.sidebar.number_input('Petal length', min_value=1.0, max_value=6.9, value=1.3, step=0.1)
+    # petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    
+    petal_width = st.sidebar.number_input('Petal width', min_value=0.1, max_value=2.5, value=0.2, step=0.1)
+    # petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    return [sepal_length,sepal_width,petal_length,petal_width]
 
 def main():
-    st.title('Iris Prediction')
-    st.title('20020541 - Quách Xuân Nam')
-    sepal_length = st.text_input('Length of sepal')
-    sepal_width = st.text_input('Width of sepal')
-    petal_length = st.text_input('Length of petal')
-    petal_width = st.text_input('Width of Petal')
+    st.image('images\\backgound.png', use_column_width='always', width=20)
+    st.markdown('<h1 style="text-align: center;">🌷 Flower Classification</h1>', unsafe_allow_html=True)
+    st.markdown('---')
+    data = input_features()
+    pred = predict(data)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown('<h1 style="text-align: center;">Image flower</h1>', unsafe_allow_html=True)
+        st.image(f'images\\{pred}_flower.png', width=500)
 
-    prediction_iris = ''
-    flower_img = ''
-    if st.button('Predict'):
-        prediction_iris,flower_img = prediction(loaded_model, sepal_length,sepal_width,petal_length,petal_width)
-        st.success(prediction_iris)
-        st.image(flower_img, width=300)
-
+    with col2:
+        st.markdown('<h1 style="text-align: center;">Prediction</h1>', unsafe_allow_html=True)
+        st.markdown(f'<h1 style="text-align: center; border: 5px solid green; ">{pred}</h1>', unsafe_allow_html=True)
+    st.caption('Modify by :blue[qxnam]')
 if __name__ == '__main__':
     main()
